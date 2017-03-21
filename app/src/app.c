67f230072f86ff2b8a76bd31a647c1a85e3884fa
cboulai <carole.boulai@ish-lyon.cnrs.fr>
2017-03-21 17:02:53 +0100

    modif cb

--- app/src/app.c
+++ app/src/app.c
@@ -6,6 +6,7 @@
 int main(int argc, char **argv) {
   fprintf(stdout, "%s", ":: welcome to [app]\n");
   fprintf(stdout, ":: number of args: %d\n", argc);
+   fprintf(stdout, ":: number of args: %d\n", argtest);
   fprintf(stdout, ":: args-pointer:   %p\n", argv);
   fprintf(stdout, "%s", ":: calling liba_hello()...\n");
   liba_hello();
